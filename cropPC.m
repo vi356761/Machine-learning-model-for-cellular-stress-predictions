585fa1345339cb5ab137b065d6f1ce139a13995e
Vignesh Aravind Subramanian Balachandar <vignesharavindsb@gmail.com>
2023-02-03 14:23:27 -0500

    Delete cropPC.m

--- cropPC.m
+++
@@ -1,7 +0,0 @@
-function [Im] = cropPC(Phase,pathname,i1,i2,j1,j2,iteration)
-
-    I = imread([pathname,Phase,num2str(iteration),'.tif']);
-    Im = I(i1:i2,j1:j2);
-    imwrite(Im,[pathname,'\Crop_PC_',num2str(iteration),'.tif']);
-  
-end
